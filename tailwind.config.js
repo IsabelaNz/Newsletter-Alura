7959dabf33c2d264528e2782b2e6ec15e268022c
Isabela Nunes <isabelanunes533905@gmail.com>
2024-05-10 11:22:07 -0300

    Newsletter

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: '#C5DFFF',
+          escuro: '#061E3C',
+          hover: '#1057B0'
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
